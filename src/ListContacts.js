2273b5b3a2be562a85bd8ec60853e72e95877fef
James Teitsworth <james@jamesteitsworth.com>
2020-06-14 12:30:29 -0500

    feat: Render default contacts

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -4,9 +4,13 @@ import React, {
 
 class ListContacts extends Component {
   render() {
-    return ( <
-      ol className = 'contact-list' >
-
+    return ( 
+      <ol className = 'contact-list' >
+        {this.props.contacts.map((contact) => (
+          <li key={contact.id}>
+            {contact.name}
+          </li>
+        ))}
       </ol>
     )
   }

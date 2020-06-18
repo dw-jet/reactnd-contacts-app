d2c0c7461f4f75abb8d3facdde74eec9a070f9c3
James Teitsworth <james@jamesteitsworth.com>
2020-06-17 21:02:14 -0500

    feat: List will filter when typing in search

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -17,21 +17,42 @@ class ListContacts extends Component {
     }))
   }
 
+  clearQuery = () => {
+    this.setState(() => ({
+      query: ''
+    }))
+  }
+
   render() {
+    const { query } = this.state
+    const { contacts, onDeleteContact } = this.props
+    const showingContacts = query === '' 
+      ? contacts 
+      : contacts.filter((c) => (
+        c.name.toLowerCase().includes(query.toLowerCase())
+      ))
     return (
       <div className='list-contacts'>
-        {JSON.stringify(this.state)}
         <div className='list-contacts-top'>
           <input
             className='search-contacts'
             type='text'
             placeholder='Search Contacts'
-            value={this.state.query}
+            value={query}
             onChange={(event) => this.updateQuery(event.target.value)}
           />
         </div>
+
+        {showingContacts.length !== contacts.length && (
+          <div className = 'showing-contacts'>
+            <span>Now showing {showingContacts.length} of {
+            contacts.length}</span>
+            <button onClick={this.clearQuery}>Show All</button>
+          </div>
+        )}
+
         <ol className = 'contact-list' >
-          {this.props.contacts.map((contact) => (
+          {showingContacts.map((contact) => (
             <li key={contact.id} className = 'contact-list-item'>
               <div
                 className='contact-avatar'
@@ -41,10 +62,10 @@ class ListContacts extends Component {
               ></div>
               <div className='contact-details'>
                 <p>{contact.name}</p>
-                <p>@{contact.handle}</p>
+                <p>{contact.handle}</p>
               </div>
               <button 
-                onClick={() => this.props.onDeleteContact(contact)}
+                onClick={() => onDeleteContact(contact)}
                 className='contact-remove'>
                 Remove
               </button>
